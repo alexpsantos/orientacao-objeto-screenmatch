44f1266088c3e7365f97b29785520f8b128358ee
Alex Santos <alex-ps@live.com>
2024-09-12 01:41:15 -0300

    herança e sobrecarga

--- src/Principal.java
+++ src/Principal.java
@@ -1,4 +1,5 @@
 import br.com.alex.screenmatch.modelo.Filme;
+import br.com.alex.screenmatch.modelo.Serie;
 
 public class Principal {
     public static void main(String[] args) {
@@ -8,8 +9,9 @@ public class Principal {
         meuFilme.setNome("Poderoso Chefão");
         meuFilme.setAnoLancamento(1970);
         meuFilme.setDuracaoEmMinutos(180);
+        System.out.println("Duração do Filme: " + meuFilme.getDuracaoEmMinutos());
 
-        meuFilme.exibirFilme();
+        meuFilme.exibirFichaTecnica();
 
         meuFilme.avaliar(8);
         meuFilme.avaliar(9);
@@ -18,6 +20,16 @@ public class Principal {
 
         System.out.println("Total de avaliações: " + meuFilme.getTotalDeAvaliacao());
         System.out.println(meuFilme.pegarMedia());
+
+        Serie minhaSerie = new Serie();
+        minhaSerie.setNome("Minha Serie");
+        minhaSerie.setAnoLancamento(1990);
+        minhaSerie.exibirFichaTecnica();
+
+        minhaSerie.setTemporadas(10);
+        minhaSerie.setEpisodiosPorTemporada(12);
+        minhaSerie.setMinutosPorEpisodio(50);
+        System.out.println("duração da serie: " + minhaSerie.getDuracaoEmMinutos());
 
     }
 }
---
+++ src/br/com/alex/screenmatch/modelo/Titulo.java
@@ -0,0 +1,65 @@
+package br.com.alex.screenmatch.modelo;
+
+public class Titulo {
+
+    private  String nome;
+    private int anoLancamento;
+    private boolean incluidoNoPlano;
+    private double somaDasAvaliacoes;
+    private int totalDeAvaliacao;
+    private int duracaoEmMinutos;
+
+
+    public void setNome (String nome){
+        this.nome = nome;
+    }
+
+    public void setIncluidoNoPlano(boolean incluidoNoPlano) {
+        this.incluidoNoPlano = incluidoNoPlano;
+    }
+
+    public void setAnoLancamento(int anoLancamento) {
+        this.anoLancamento = anoLancamento;
+    }
+
+    public void setDuracaoEmMinutos(int duracaoEmMinutos) {
+        this.duracaoEmMinutos = duracaoEmMinutos;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public int getAnoLancamento() {
+        return anoLancamento;
+    }
+
+    public boolean isIncluidoNoPlano() {
+        return incluidoNoPlano;
+    }
+
+    public int getDuracaoEmMinutos() {
+        return duracaoEmMinutos;
+    }
+
+    public int getTotalDeAvaliacao() {
+        return totalDeAvaliacao;
+    }
+
+
+    public void exibirFichaTecnica(){
+        System.out.println("Nome: " + nome);
+        System.out.println("Ano: " + anoLancamento);
+    }
+
+    public void avaliar(double nota) {
+        somaDasAvaliacoes += nota;
+        totalDeAvaliacao++;
+    }
+
+    public double pegarMedia(){
+        return somaDasAvaliacoes / totalDeAvaliacao;
+    }
+
+
+}
