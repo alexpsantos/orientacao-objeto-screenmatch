4829fef69fdcf0fc6efefcda40202cdfd7517cf1
Alex Santos <alex-ps@live.com>
2024-09-11 21:29:57 -0300

    criando pacote e encapsulando

--- src/Principal.java
+++ src/Principal.java
@@ -1,11 +1,13 @@
+import br.com.alex.screenmatch.modelo.Filme;
+
 public class Principal {
     public static void main(String[] args) {
 
         Filme meuFilme = new Filme();
 
-        meuFilme.nome = "Poderoso Chefão";
-        meuFilme.anoLancamento = 1970;
-        meuFilme.duracaoEmMinutos = 180;
+        meuFilme.setNome("Poderoso Chefão");
+        meuFilme.setAnoLancamento(1970);
+        meuFilme.setDuracaoEmMinutos(180);
 
         meuFilme.exibirFilme();
 
@@ -13,8 +15,8 @@ public class Principal {
         meuFilme.avaliar(9);
         meuFilme.avaliar(6);
 
-        System.out.println(meuFilme.somaDasAvaliacoes);
-        System.out.println(meuFilme.totalDeAvaliacao);
+
+        System.out.println("Total de avaliações: " + meuFilme.getTotalDeAvaliacao());
         System.out.println(meuFilme.pegarMedia());
 
     }
